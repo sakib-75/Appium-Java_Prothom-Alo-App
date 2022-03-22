0040d7958bdb7efd0d2dc188c450af005b3c83c4
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-22 18:00:22 +0600

    Search update

--- src/test/java/search/VoiceSearch.java
+++ src/test/java/search/VoiceSearch.java
@@ -2,10 +2,16 @@ package search;
 
 import home.Home;
 import org.testng.annotations.Test;
+import page_object_model.Search_POM;
 
 public class VoiceSearch extends Home {
     @Test(priority = 2)
     public void voiceSearch(){
 
+        Search_POM pom = new Search_POM(driver);
+        pom.search().click();
+        pom.voiceSearch().click();
+
     }
+
 }

98cb9df1764ce6e9d46ae1c0fc5461b748a833f5
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-03-30 18:16:57 +0600

    Base class update

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -17,17 +17,23 @@ public class BaseClass {
     public static AndroidDriver setup() {
 
         DesiredCapabilities cap = new DesiredCapabilities();
-        // APK file path
-        File appDir = new File("src/main/resources");
-        File app = new File(appDir, "prothomalo.apk");
 
         cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "12");
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "realme 8");
         cap.setCapability(MobileCapabilityType.UDID, "VKKNV8S8FYYXLRH6");
 
+        cap.setCapability("appPackage", "com.mcc.prothomalo");
+        cap.setCapability("appActivity", "com.quintype.prothomalo.main.activities.MainActivity");
+
+        /*
+        APK file path
+        File appDir = new File("src/main/resources");
+        File app = new File(appDir, "prothomalo.apk");
         cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        */
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
         // Appium server
         URL url = null;
         try {
